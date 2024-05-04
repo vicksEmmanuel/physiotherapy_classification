a2979510c381a0723481ac52a2b6038a509d333b
Victor Umesiobi <Vicksemmanuel58@gmail.com>
2024-05-05 00:29:49 +0100

    regex json fix

--- api/myapp.py
+++ api/myapp.py
@@ -17,13 +17,15 @@ from runner.test import get_new_data_from_video
 
 def extract_json(text):
     json_pattern = re.compile(r'{.*}', re.DOTALL)
-    text = text.replace("\\n", "").replace("\\", "")
+    text = text.replace("\\\\n", "").replace("\n","").replace("\\\\", "\\")
     json_match = json_pattern.search(text)
     if json_match:
         json_string = json_match.group()
-        json_string = re.sub(r'(?<!\\)"', '\\"', json_string)
-        json_data = json.loads(json_string)
-        return json_data
+        try:
+            json_data = json.loads(json_string)
+            return json_data
+        except json.JSONDecodeError:
+            return None
     else:
         return None
 
